82edf942c2ae9bb418c85d93f73bb132b822d773
KalaivaniVelusamy <kalai.v.vani@gmail.com>
2021-10-27 22:13:50 +0530

    Added Project

---
+++ JobSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JobSearch
+//
+//  Created by V, Kalaivani V. (623-Extern) on 27/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
