6d1676de10c732fc480764c5ebf1ea587d914aab
KalaivaniVelusamy <kalai.v.vani@gmail.com>
2021-10-28 21:29:27 +0530

    added service layer

---
+++ JobSearch/Service/ServiceManager.swift
@@ -0,0 +1,14 @@
+//
+//  ServiceManager.swift
+//  JobSearch
+//
+//  Created by V, Kalaivani V. (623-Extern) on 28/10/21.
+//
+
+import Foundation
+
+class ServiceManager {
+    
+    func getResults() {
+    }
+}
